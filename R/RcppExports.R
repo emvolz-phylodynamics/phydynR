a4cb82e4a3e518739c46208f9e90eef2f6ff00d4
emvolz <erik.volz@gmail.com>
2019-01-23 18:31:18 +0000

    names

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -1,6 +1,6 @@
 
 dAL <- function(t, y, parms) {
-	.Call('sourceCpp_0_dAL',  PACKAGE='phydynR.r0.1.2', t, y, parms)
+	.Call('sourceCpp_0_dAL',  PACKAGE='phydynR', t, y, parms)
 }
 
 simulateTreeCpp2 <- function(times
@@ -15,7 +15,7 @@ simulateTreeCpp2 <- function(times
 		 , m
 		 , finiteSizeCorrection
 		 , DEMES) {
-	.Call('sourceCpp_2_simulateTreeCpp2',  PACKAGE='phydynR.r0.1.2', times
+	.Call('sourceCpp_2_simulateTreeCpp2',  PACKAGE='phydynR', times
 		 ,  Fs
 		 ,  Gs
 		 ,  Ys
@@ -41,7 +41,7 @@ simulateTreeCpp3x0 <- function(times
 		 , substitutionRates
 		 , sequenceLength
 ) {
-	.Call('sourceCpp_simulateTreeCpp3x0',  PACKAGE='phydynR.r0.1.2'
+	.Call('sourceCpp_simulateTreeCpp3x0',  PACKAGE='phydynR'
 		 , times
 		 ,  Fs
 		 ,  Gs
@@ -61,14 +61,14 @@ simulateTreeCpp3x0 <- function(times
 #########################
 
 sourceAttribMultiDemeCpp <- function(heights, Fs, Gs, Ys, eventIndicator, eventIndicatorNode, eventHeights, sortedSampleStates, daughters, n, Nnode, m, AgtYboundaryCondition, maxHeight) {
-	.Call(  'sourceCpp_0_sourceAttribMultiDemeCpp', PACKAGE='phydynR.r0.1.2'
+	.Call(  'sourceCpp_0_sourceAttribMultiDemeCpp', PACKAGE='phydynR'
 	 , heights, Fs, Gs, Ys, eventIndicator, eventIndicatorNode, eventHeights, sortedSampleStates, daughters, n, Nnode, m, AgtYboundaryCondition, maxHeight
 	)
 }
 
 
 sourceAttribMultiDemeCpp2 <- function(heights, Fs, Gs, Ys, eventIndicator, eventIndicatorNode, eventHeights, sortedSampleStates, daughters, n, Nnode, m, AgtYboundaryCondition, maxHeight, step_size_res) {
-	.Call(  'sourceCpp_1_sourceAttribMultiDemeCpp2', PACKAGE='phydynR.r0.1.2'
+	.Call(  'sourceCpp_1_sourceAttribMultiDemeCpp2', PACKAGE='phydynR'
 	 , heights, Fs, Gs, Ys, eventIndicator, eventIndicatorNode, eventHeights, sortedSampleStates, daughters, n, Nnode, m, AgtYboundaryCondition, maxHeight, step_size_res
 	)
 }
@@ -78,57 +78,57 @@ sourceAttribMultiDemeCpp2 <- function(heights, Fs, Gs, Ys, eventIndicator, event
 
 ############# colik modular stuff
 rco_finite_size_correction2 <- function(a, p_a, A, extantLines, mstates) {
-	.Call( 'sourceCpp_0_rco_finite_size_correction2', PACKAGE='phydynR.r0.1.2'
+	.Call( 'sourceCpp_0_rco_finite_size_correction2', PACKAGE='phydynR'
 	, a, p_a, A, extantLines, mstates)
 }
 eventTimes2extant <- function(eventTimes, nodeheights, parentheights) {
-	.Call( 'sourceCpp_0_eventTimes2extant' , PACKAGE='phydynR.r0.1.2'
+	.Call( 'sourceCpp_0_eventTimes2extant' , PACKAGE='phydynR'
 	, eventTimes, nodeheights, parentheights)
 }
 update_alpha0 <- function(pu, pv, Fmat, Yvec, A) {
-	.Call(  'sourceCpp_0_update_alpha0', PACKAGE='phydynR.r0.1.2'
+	.Call(  'sourceCpp_0_update_alpha0', PACKAGE='phydynR'
 	,pu, pv, Fmat, Yvec, A)
 }
 update_states0 <- function(mstates, Q) {
-	.Call(  'sourceCpp_0_update_states0', PACKAGE='phydynR.r0.1.2'
+	.Call(  'sourceCpp_0_update_states0', PACKAGE='phydynR'
 	 , mstates, Q)
 }
 update_states1 <- function(mstates, Q, el) {
-	.Call(  'sourceCpp_0_update_states1', PACKAGE='phydynR.r0.1.2'
+	.Call(  'sourceCpp_0_update_states1', PACKAGE='phydynR'
 	 , mstates, Q, el)
 }
 solveQALboost0 <- function(times, Fs, Gs, Ys, h0, h1, L0, A0) {
-	.Call( 'sourceCpp_2_solveQALboost0' , PACKAGE='phydynR.r0.1.2'
+	.Call( 'sourceCpp_2_solveQALboost0' , PACKAGE='phydynR'
 	 , times, Fs, Gs, Ys, h0, h1, L0, A0)
 }
 
 solveQALboost1 <- function(times, Fs, Gs, Ys, h0, h1, L0, A0) {
-	.Call( 'sourceCpp_solveQALboost1' , PACKAGE='phydynR.r0.1.2'
+	.Call( 'sourceCpp_solveQALboost1' , PACKAGE='phydynR'
 	 , times, Fs, Gs, Ys, h0, h1, L0, A0)
 }
 
 
 dQLcpp <- function ( x, FF, GG, YY, A0 ){
-	.Call( 'scpp_dQL', PACKAGE='phydynR.r0.1.2'
+	.Call( 'scpp_dQL', PACKAGE='phydynR'
 	 , x, FF , GG , YY, A0 )
 }
 
 ############################
 
 solvePikL0 <- function( times, Fs, Gs, Ys, h0, h1, pik0, step_size_res = 10) {
-	.Call( 'sourceCpp_1_solvePikL0', PACKAGE='phydynR.r0.1.2', times, Fs, Gs, Ys, h0, h1, pik0, step_size_res )
+	.Call( 'sourceCpp_1_solvePikL0', PACKAGE='phydynR', times, Fs, Gs, Ys, h0, h1, pik0, step_size_res )
 }
 
 ############################
 
 solvePikL1 <- function( times, Fs, Gs, Ys, h0, h1, pik0, step_size_res = 10) {
-	.Call( 'sourceCpp_1_solvePikL1', PACKAGE='phydynR.r0.1.2', times, Fs, Gs, Ys, h0, h1, pik0, step_size_res )
+	.Call( 'sourceCpp_1_solvePikL1', PACKAGE='phydynR', times, Fs, Gs, Ys, h0, h1, pik0, step_size_res )
 }
 
 
 ############################
 
 solveQfwd0 <- function(times, Fs, Gs, Ys, deaths, m, h1, h0) {
-	.Call('sourceCpp_1_solveQfwd0', PACKAGE='phydynR.r0.1.2', times, Fs, Gs, Ys, deaths, m, h1, h0 )
+	.Call('sourceCpp_1_solveQfwd0', PACKAGE='phydynR', times, Fs, Gs, Ys, deaths, m, h1, h0 )
 }
 
